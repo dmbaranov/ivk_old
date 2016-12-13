bf124cfd727556258a376da1699f3ec806611080
Dmitriy Baranov <dm.baranov@rambler-co.ru>
2016-12-13 14:41:07 +0300

    Input for the messages

--- app/containers/MessagesContainer.js
+++ app/containers/MessagesContainer.js
@@ -2,6 +2,9 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import ReactDOM from 'react-dom';
 import RefreshIndicator from 'material-ui/RefreshIndicator';
+import TextField from 'material-ui/TextField';
+import IconButton from 'material-ui/IconButton';
+import Send from 'material-ui/svg-icons/content/send';
 
 import Message from 'app/components/Message';
 
@@ -27,7 +30,7 @@ export class MessagesContainer extends Component {
   componentDidUpdate() {
     // TODO: Try to find a better solution to scroll to the bottom of the page
     if (this.refs.container) {
-      $(ReactDOM.findDOMNode(this.refs.container)).animate({scrollTop: 10000}, 1);
+      $(ReactDOM.findDOMNode(this.refs.container)).animate({scrollTop: 10000}, 100);
     }
   }
 
@@ -36,7 +39,6 @@ export class MessagesContainer extends Component {
     return this.props.dialogs.dialog.map((item, index) => {
       return (
         <Message
-          ref={index === this.props.dialogs.dialog.length - 1 ? 'lastElement' : ''}
           key={index}
           user={item.user}
           body={item.body}
@@ -53,19 +55,55 @@ export class MessagesContainer extends Component {
 
     if (isPending === true) {
       return (
-        <div>
+        <div className={styles.indicator}>
           <RefreshIndicator
-            className={styles.indicator}
             style={indicatorStyles}
             size={50}
             status="loading"
             top={0}
             left={0}/>
         </div>
-      )
+      );
     }
     else {
-      return this.renderMessages();
+      return (
+        <div className={styles.messagesContainer}>
+          {this.renderMessages()}
+          <div className={styles.inputContainer}>
+            <TextField
+              floatingLabelText="Введите сообщение"
+              multiLine={true}
+              rows={4}
+              rowsMax={4}
+              fullWidth={true}/>
+            <IconButton
+              style={{
+                alignSelf: 'center',
+                width: 120,
+                height: 120,
+                padding: 30
+              }}
+              iconStyle={{
+                width: 60,
+                height: 60
+              }}>
+              <Send />
+            </IconButton>
+          </div>
+        </div>
+      );
+      // return (
+      //   <div>
+      //     {this.renderMessages()}
+      //     <div className={styles.inputContainer}>
+      //       <TextField
+      //         floatingLabelText="Введите сообщение"
+      //         multiLine={true}
+      //         rowsMax={2}
+      //         fullWidth={true}/>
+      //     </div>
+      //   </div>
+      // );
     }
   }
 
